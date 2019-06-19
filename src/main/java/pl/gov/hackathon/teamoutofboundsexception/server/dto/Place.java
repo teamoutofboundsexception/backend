2762920485766eaf7b328fdfe9301f9cf0e89f18
nostressghost <andrzej1.95@o2.pl>
2019-06-19 14:43:31 +0200

    First parser added

---
+++ src/main/java/pl/gov/hackathon/teamoutofboundsexception/server/dto/Place.java
@@ -0,0 +1,38 @@
+package pl.gov.hackathon.teamoutofboundsexception.server.dto;
+
+public class Place {
+
+    protected Integer placeId;
+    protected Integer cityId;
+    protected Integer placeTypeId;
+    protected String placeName;
+    protected Float mapX;
+    protected Float mapY;
+    protected String streetName;
+    protected String houseNumber;
+    protected Integer apartmentNumber;
+    protected Float normalAVGPrice;
+
+    public Place(Integer placeId, Integer cityId, Integer placeTypeId, String placeName, Float mapX, Float mapY, String streetName,
+                 String houseNumber, Integer apartmentNumber, Float normalAVGPrice) {
+        this.placeId = placeId;
+        this.cityId = cityId;
+        this.placeTypeId = placeTypeId;
+        this.placeName = placeName;
+        this.mapX = mapX;
+        this.mapY = mapY;
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+        this.apartmentNumber = apartmentNumber;
+        this.normalAVGPrice = normalAVGPrice;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public String createRecordCSV(char delimeter) {
+        return "" + placeId + delimeter + cityId + delimeter + placeTypeId + delimeter + mapX + delimeter + mapY
+                + delimeter + streetName + delimeter + houseNumber + delimeter + normalAVGPrice;
+    }
+}
